f279f4daab840d52e00d89a0ade5bdf4490c57fd
aryan_mor <aryanmoradi74@gmail.com>
2021-07-30 13:08:38 +0430

    Fucking bug fixed -_-

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,56 @@
 import React from 'react'
 
-import { ExampleComponent } from 'smart-image-lazyload'
+import { Img } from 'smart-image-lazyload'
 import 'smart-image-lazyload/dist/index.css'
 
+
+
+const testImages = [
+  {
+    src: 'https://api.mehrtakhfif.com/media/boxes/2/2020-07-31/media/08-14-40-23-has-ph.jpg',
+    ph: 'https://api.mehrtakhfif.com/media/boxes/2/2020-07-31/media/08-14-40-23-ph.jpg'
+  },
+  {
+    src: 'https://api.mehrtakhfif.com/media/boxes/2/2020-07-31/media/11-30-43-13-has-ph.jpg',
+    ph: 'https://api.mehrtakhfif.com/media/boxes/2/2020-07-31/media/11-30-43-13-ph.jpg'
+  },
+  {
+    src: 'https://api.mehrtakhfif.com/media/boxes/2/2020-11-17/media/22-39-33-66-has-ph.jpg',
+    ph: 'https://api.mehrtakhfif.com/media/boxes/2/2020-11-17/media/22-39-33-66-ph.jpg'
+  },
+  {
+    src: 'https://api.mehrtakhfif.com/media/boxes/2/2020-12-07/media/14-10-09-34-has-ph.jpg',
+    ph: 'https://api.mehrtakhfif.com/media/boxes/2/2020-12-07/media/14-10-09-34-ph.jpg'
+  },
+  {
+    src: 'https://api.mehrtakhfif.com/media/boxes/2/2021-02-14/media/09-43-53-54-has-ph.jpg',
+    ph: 'https://api.mehrtakhfif.com/media/boxes/2/2021-02-14/media/09-43-53-54-ph.jpg'
+  }
+]
+
+
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <div>
+      <div style={{
+        display:'flex',
+        flexWrap:'wrap'
+      }}>
+        {
+          [testImages[0]].map(({ src, ph },index) => (
+            <div key={index} style={{ padding:20,width:"20%"}}>
+              <Img
+                imageWidth={1280}
+                imageHeight={794}
+                src={src}
+                placeholderSrc={ph}
+                alt={index + ' image'}/>
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,188 @@
-import React from 'react'
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
 import styles from './styles.module.css'
+// import PropTypes from 'prop-types'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const Img = (pr) => {
+  const {
+    skeleton,
+    imageWidth,
+    imageHeight,
+    src,
+    placeholderSrc,
+    alt,
+    loading,
+    imageRootProps = {},
+    imageProps = {},
+    ...props
+  } = pr
+  const [width] = useWindowSize()
+  const ref = useRef()
+  const [loaded, setLoaded] = useState(false)
+  const [size, setSize] = useState()
+
+  useEffect(() => {
+    setImageSize()
+  }, [width])
+
+  function setImageSize() {
+    if (!(imageWidth && imageHeight)) return
+
+    const process = () => {
+      const [width, height] = getImageSize(ref, imageWidth, imageHeight)
+      setSize({
+        width,
+        height
+      })
+    }
+
+    if (size) {
+      setSize(undefined)
+      setTimeout(process, 300)
+      return
+    }
+    process()
+  }
+
+  useEffect(() => {
+    let interval
+    if (!loaded) {
+      try {
+        setTimeout(() => {
+          interval = setInterval(() => {
+            if (!(ref && ref.current)) return
+            if (
+              loaded ||
+              ref.current.getElementsByClassName('smart-image')[0].complete
+            ) {
+              setLoaded(true)
+              clearInterval(interval)
+            }
+          }, 400)
+        }, 1000)
+      } catch {}
+    }
+    return () => {
+      clearInterval(interval)
+    }
+  }, [loaded])
+
+  return (
+    <div
+      ref={ref}
+      {...props}
+      style={{
+        width: size?.width || '100%',
+        height: size?.height,
+        position: 'relative',
+        ...props?.style
+      }}
+    >
+      {!loaded &&
+        (skeleton && React.isValidElement(skeleton) ? (
+          skeleton
+        ) : (
+          <div className={styles.skeleton} />
+        ))}
+      <div
+        {...imageRootProps}
+        style={{
+          width: size?.width || '100%',
+          height: size?.height,
+          ...(placeholderSrc
+            ? {
+                backgroundImage: `url("${placeholderSrc}")`,
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover'
+              }
+            : {}),
+          ...imageRootProps?.style
+        }}
+      >
+        <img
+          className={`smart-image ${styles.initWithOpacity} ${
+            loaded ? styles.initWithOpacityStart : ''
+          }`}
+          src={src}
+          alt={alt}
+          loading={loading}
+          onLoad={() => {
+            setLoaded(true)
+          }}
+          style={{
+            width: size?.width || '100%',
+            height: size?.height,
+            ...imageProps
+          }}
+        />
+      </div>
+    </div>
+  )
 }
+
+export function useWindowSize(wait = 2000) {
+  const [size, setSize] = useState([0, 0])
+
+  useLayoutEffect(() => {
+    updateSize()
+    try {
+      window?.addEventListener(
+        'resize',
+        debounce(function () {
+          updateSize()
+        }, wait)
+      )
+      return () => window?.removeEventListener('resize', updateSize)
+    } catch {}
+  }, [])
+
+  function updateSize() {
+    try {
+      setSize([window?.innerWidth, window?.innerHeight])
+    } catch {}
+  }
+
+  return size
+}
+
+export function getImageSize(ref, imageWidth, imageHeight) {
+  try {
+    const offsetWidth = ref?.current?.offsetWidth
+    const height = (offsetWidth * imageHeight) / imageWidth
+    return [offsetWidth, height]
+  } catch (e) {}
+  return [imageWidth, imageHeight]
+}
+
+function debounce(func, wait, immediate) {
+  let timeout
+  return function () {
+    const context = this
+    const args = arguments
+    const later = function () {
+      timeout = null
+      if (!immediate) func.apply(context, args)
+    }
+    const callNow = immediate && !timeout
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+    if (callNow) func.apply(context, args)
+  }
+}
+
+//
+// Img.defaultProps = {
+//   loading: 'lazy'
+// }
+//
+// Img.propTypes = {
+//   imageWidth: PropTypes.number,
+//   imageHeight: PropTypes.number,
+//   src: PropTypes.string,
+//   placeholderSrc: PropTypes.string,
+//   alt: PropTypes.string,
+//   loading: PropTypes.string,
+//   imageRootProps: PropTypes.object,
+//   imageProps: PropTypes.object,
+//   skeleton: PropTypes.oneOfType([PropTypes.element, PropTypes.bool])
+// }

7aa6cf8b307103595cf17e6d3dd92aa1d31c31c6
aryan_mor <aryanmoradi74@gmail.com>
2021-08-15 12:19:51 +0430

    Add crs to if isLoaded

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
-import styles from './styles.module.css'
-import PropTypes from 'prop-types'
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import styles from "./styles.module.css";
+import PropTypes from "prop-types";
 
 export const Img = (pr) => {
   const {
@@ -14,176 +14,179 @@ export const Img = (pr) => {
     imageRootProps = {},
     imageProps = {},
     ...props
-  } = pr
-  const [width] = useWindowSize()
-  const ref = useRef()
-  const [loaded, setLoaded] = useState(false)
-  const [error, setError] = useState(false)
-  const [size, setSize] = useState()
+  } = pr;
+  const [width] = useWindowSize();
+  const ref = useRef();
+  const [loaded, setLoaded] = useState(false);
+  const [error, setError] = useState(false);
+  const [size, setSize] = useState();
 
   useEffect(() => {
-    setImageSize()
-  }, [width])
+    setImageSize();
+  }, [width]);
 
   function setImageSize() {
-    if (!(imageWidth && imageHeight)) return
+    if (!(imageWidth && imageHeight)) return;
 
     const process = () => {
-      const [width, height] = getImageSize(ref, imageWidth, imageHeight)
+      const [width, height] = getImageSize(ref, imageWidth, imageHeight);
       setSize({
         width,
         height
-      })
-    }
+      });
+    };
 
     if (size) {
-      setSize(undefined)
-      setTimeout(process, 300)
-      return
+      setSize(undefined);
+      setTimeout(process, 300);
+      return;
     }
-    process()
+    process();
   }
 
   useEffect(() => {
-    let interval
+    let interval;
     if (!loaded) {
       try {
         setTimeout(() => {
           interval = setInterval(() => {
-            if (!(ref && ref.current)) return
+            if (!(ref && ref.current)) return;
             if (
               loaded ||
-              ref.current.getElementsByClassName('smart-image')[0].complete
+              ref.current.getElementsByClassName("smart-image")[0].complete
             ) {
-              setLoaded(true)
-              clearInterval(interval)
+              setLoaded(true);
+              clearInterval(interval);
             }
-          }, 400)
-        }, 1000)
-      } catch {}
+          }, 400);
+        }, 1000);
+      } catch {
+      }
     }
     return () => {
-      clearInterval(interval)
-    }
-  }, [loaded])
+      clearInterval(interval);
+    };
+  }, [loaded]);
 
 
-
-  const isLoaded = loaded && !error
+  const isLoaded = src && loaded && !error;
 
   return (
     <div
       ref={ref}
       {...props}
       style={{
-        maxWidth:'100%',
-        width: size?.width || '100%',
+        maxWidth: "100%",
+        width: size?.width || "100%",
         height: size?.height,
-        position: 'relative',
+        position: "relative",
         ...props?.style
       }}>
       {!isLoaded &&
-        (skeleton && React.isValidElement(skeleton) ? (
-          skeleton
-        ) : (
-          <div className={styles.skeleton} />
-        ))}
+      (skeleton && React.isValidElement(skeleton) ? (
+        skeleton
+      ) : (
+        <div className={styles.skeleton} />
+      ))}
       <div
         {...imageRootProps}
         style={{
-          maxWidth:'100%',
-          width: size?.width || '100%',
+          maxWidth: "100%",
+          width: size?.width || "100%",
           height: size?.height,
           ...(placeholderSrc
             ? {
-                backgroundImage: `url("${placeholderSrc}")`,
-                backgroundPosition: 'center',
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'cover'
-              }
+              backgroundImage: `url("${placeholderSrc}")`,
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover"
+            }
             : {}),
           ...imageRootProps?.style
         }}>
         <img
           className={`smart-image ${styles.initWithOpacity} ${
-            isLoaded ? styles.initWithOpacityStart : ''
+            isLoaded ? styles.initWithOpacityStart : ""
           }`}
           src={src}
           alt={alt}
           loading={loading}
           onLoad={() => {
-            setLoaded(true)
+            setLoaded(true);
           }}
           onError={() => {
-            setError(true)
+            setError(true);
           }}
           {...imageProps}
           style={{
-            maxWidth:'100%',
-            width: size?.width || '100%',
+            maxWidth: "100%",
+            width: size?.width || "100%",
             height: size?.height,
             ...imageProps?.style
           }}
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export function useWindowSize(wait = 2000) {
-  const [size, setSize] = useState([0, 0])
+  const [size, setSize] = useState([0, 0]);
 
   useLayoutEffect(() => {
-    updateSize()
+    updateSize();
     try {
       window?.addEventListener(
-        'resize',
-        debounce(function () {
-          updateSize()
+        "resize",
+        debounce(function() {
+          updateSize();
         }, wait)
-      )
-      return () => window?.removeEventListener('resize', updateSize)
-    } catch {}
-  }, [])
+      );
+      return () => window?.removeEventListener("resize", updateSize);
+    } catch {
+    }
+  }, []);
 
   function updateSize() {
     try {
-      setSize([window?.innerWidth, window?.innerHeight])
-    } catch {}
+      setSize([window?.innerWidth, window?.innerHeight]);
+    } catch {
+    }
   }
 
-  return size
+  return size;
 }
 
 export function getImageSize(ref, imageWidth, imageHeight) {
   try {
-    const offsetWidth = ref?.current?.offsetWidth
-    const height = (offsetWidth * imageHeight) / imageWidth
-    return [offsetWidth, height]
-  } catch (e) {}
-  return [imageWidth, imageHeight]
+    const offsetWidth = ref?.current?.offsetWidth;
+    const height = (offsetWidth * imageHeight) / imageWidth;
+    return [offsetWidth, height];
+  } catch (e) {
+  }
+  return [imageWidth, imageHeight];
 }
 
 function debounce(func, wait, immediate) {
-  let timeout
-  return function () {
-    const context = this
-    const args = arguments
-    const later = function () {
-      timeout = null
-      if (!immediate) func.apply(context, args)
-    }
-    const callNow = immediate && !timeout
-    clearTimeout(timeout)
-    timeout = setTimeout(later, wait)
-    if (callNow) func.apply(context, args)
-  }
+  let timeout;
+  return function() {
+    const context = this;
+    const args = arguments;
+    const later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
 }
 
 
 Img.defaultProps = {
-  loading: 'lazy'
-}
+  loading: "lazy"
+};
 
 Img.propTypes = {
   imageWidth: PropTypes.number,
@@ -195,4 +198,4 @@ Img.propTypes = {
   imageRootProps: PropTypes.object,
   imageProps: PropTypes.object,
   skeleton: PropTypes.oneOfType([PropTypes.element, PropTypes.bool])
-}
+};
